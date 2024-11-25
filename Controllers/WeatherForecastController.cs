3aea070a7726eb6232c2c789057a371031eb720f
Matthias Reynders <matthias.reynders3@gmail.com>
2024-11-25 15:41:56 +0100

    test

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace debt_collector_api.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Frost"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
