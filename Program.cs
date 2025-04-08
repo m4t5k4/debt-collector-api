3409f6c6baab91b2792f98a9915b4f0c39b9a97f
Matthias Reynders <matthias.reynders3@gmail.com>
2025-04-08 16:41:36 +0200

    datetime

--- Program.cs
+++ Program.cs
@@ -14,6 +14,7 @@ builder.Services.AddControllers().AddJsonOptions(options =>
 {
     options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
     options.JsonSerializerOptions.WriteIndented = true;
+    options.JsonSerializerOptions.Converters.Add(new JsonDateTimeConverter());
 });
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
